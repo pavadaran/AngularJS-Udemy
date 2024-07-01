9b8a6ec1c76fc8039921554bbf15d84b699c8b62
pavadaran <ppavadaran@isa.ae>
2024-07-01 17:52:06 +0530

    Bonus lessons

--- Weather App/controllers.js
+++ Weather App/controllers.js
@@ -1,23 +1,23 @@
 // CONTROLLERS
-weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('homeController', ['$scope', '$location', 'cityService', 
+    function($scope, $location, cityService) {
     $scope.city = cityService.city;
 
     $scope.$watch('city', function() {
         cityService.city = $scope.city;
     });
+
+    $scope.submit = function() {
+        $location.path("/forecast");
+    }
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$routeParams', 'cityService', 'weatherService', 
+    function($scope, $routeParams, cityService, weatherService) {
     $scope.city = cityService.city;
     $scope.days = $routeParams.days || '2';
 
-    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast", {get: {method: "JSONP"}});
-
-    $scope.weatherResult = $scope.weatherAPI.get({ 
-        q: $scope.city, 
-        cnt: $scope.days,
-        appid: '8ff224cf7b65122a60a0c615d9721e22'
-    });
+    $scope.weatherResult = weatherService.GetWeather($scope.city, $scope.days);
     
     $scope.convertToCelcius = function(degK) {
         return Math.round(degK - 273.15);
--- Weather App/services.js
+++ Weather App/services.js
@@ -1,4 +1,16 @@
 // SERVICES
-weatherApp.service('cityService', function() {
+weatherApp.service('cityService', function () {
     this.city = "New York";
 });
+
+weatherApp.service('weatherService', ['$resource', function ($resource) {
+    this.GetWeather = function (city, days) {
+        var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast", { get: { method: "JSONP" } });
+
+        return weatherAPI.get({
+            q: city,
+            cnt: days,
+            appid: '8ff224cf7b65122a60a0c615d9721e22'
+        });
+    }
+}]);
